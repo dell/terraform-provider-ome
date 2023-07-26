3c9da0fc3e7075b8b7027f8609b062aacece7fb0
Krishnan-Priyanshu <Priyanshu.Krishnan@dell.com>
2023-07-26 04:43:23 -0400

    add discovery schema

---
+++ ome/resource_discovery.go
@@ -0,0 +1,122 @@
+package ome
+
+import (
+	"context"
+	"terraform-provider-ome/models"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ resource.Resource = &discoveryResource{}
+)
+
+// NewdiscoveryResource is a helper function to simplify the provider implementation.
+func NewdiscoveryResource() resource.Resource {
+	return &discoveryResource{}
+}
+
+// discoveryResource is the resource implementation.
+type discoveryResource struct {
+	p *omeProvider
+}
+
+// Configure implements resource.ResourceWithConfigure
+func (r *discoveryResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+	r.p = req.ProviderData.(*omeProvider)
+}
+
+// Metadata returns the resource type name.
+func (r *discoveryResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_discovery"
+}
+
+// Schema defines the schema for the resource.
+func (r *discoveryResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Resource for managing discovery on OpenManage Enterprise.",
+		Version:             1,
+		Attributes:          OmeDiscoveryJobSchema(),
+	}
+}
+
+// Create creates the resource and sets the initial Terraform state.
+func (r *discoveryResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	tflog.Trace(ctx, "resource_discovery create : Started")
+	//Get Plan Data
+	var plan, state models.OmeDiscoveryJob
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Trace(ctx, "resource_discovery create: updating state finished, saving ...")
+	// Save into State
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	tflog.Trace(ctx, "resource_discovery create: finish")
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *discoveryResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	tflog.Trace(ctx, "resource_discovery read: started")
+	var state models.OmeDiscoveryJob
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Trace(ctx, "resource_discovery read: finished reading state")
+	//Save into State
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	tflog.Trace(ctx, "resource_discovery read: finished")
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *discoveryResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	//Get state Data
+	tflog.Trace(ctx, "resource_discovery update: started")
+	var state, plan models.OmeDiscoveryJob
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get plan Data
+	diags = req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Trace(ctx, "resource_discovery update: finished state update")
+	//Save into State
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	tflog.Trace(ctx, "resource_discovery update: finished")
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *discoveryResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	tflog.Trace(ctx, "resource_discovery delete: started")
+	// Get State Data
+	var state models.OmeDiscoveryJob
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.State.RemoveResource(ctx)
+	tflog.Trace(ctx, "resource_discovery delete: finished")
+}
