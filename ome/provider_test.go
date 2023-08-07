5465c35e1660f1a84be8200e9b0a574b081e24df
Rounak Adhikary <Rounak.Adhikary@dell.com>
2023-08-07 06:16:11 -0400

    Removing duplicate in merge

--- ome/provider_test.go
+++ ome/provider_test.go
@@ -38,7 +38,6 @@ var DeviceSvcTag1 = os.Getenv("DEVICESVCTAG1")
 var DeviceSvcTag2 = os.Getenv("DEVICESVCTAG2")
 var GroupID1 = os.Getenv("GROUPID1")
 var DeviceID1 = os.Getenv("DEVICEID1")
-var DeviceIP1 = os.Getenv("DEVICEIP1")
 var DeviceID2 = os.Getenv("DEVICEID2")
 var DeviceID3 = os.Getenv("DEVICEID3") // Not capable for deployment
 var ShareUser = os.Getenv("SHAREUSERNAME")
