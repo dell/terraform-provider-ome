08fbe6a423d8730d54143d95f3d8ad91a3a3d3d5
Rounak Adhikary <Rounak.Adhikary@dell.com>
2023-09-05 10:23:09 -0400

    Make device_ids optional

--- ome/resource_static_group.go
+++ ome/resource_static_group.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -113,8 +114,12 @@ func (r resourceStaticGroup) Schema(_ context.Context, _ resource.SchemaRequest,
 			"device_ids": schema.SetAttribute{
 				MarkdownDescription: "Device ids in the group",
 				Description:         "Device ids in the group",
-				Required:            true,
+				Optional:            true,
+				Computed:            true,
 				ElementType:         types.Int64Type,
+				PlanModifiers: []planmodifier.Set{
+					setplanmodifier.UseStateForUnknown(),
+				},
 			},
 		},
 	}
