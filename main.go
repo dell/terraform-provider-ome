38f541071304062481d1103be4f89036fb8542fa
Connor Doria <128822122+doriac11@users.noreply.github.com>
2025-04-04 09:35:03 -0400

    Add Environment Provider Variables (#145)

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+/*
+Copyright (c) 2024-2025 Dell Inc., or its subsidiaries. All Rights Reserved.
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://mozilla.org/MPL/2.0/
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -9,10 +24,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --rendered-website-dir docs --provider-name terraform-provider-ome
-// var (
-// 	version string = "dev"
-// )
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@v0.19.4 generate --provider-name terraform-provider-ome
 
 func main() {
 	var debug bool
